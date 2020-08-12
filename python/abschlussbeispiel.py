6c8ade4d0058cd4d3ceefe57540fdf572f451ad5
peeterburger <peter2000.burger2000@gmail.com>
2020-08-12 14:19:04 +0200

    minor code readability improvements

--- python/abschlussbeispiel.py
+++ python/abschlussbeispiel.py
@@ -2,8 +2,7 @@
 # Gruppe [Namen bitte einfügen]
 
 # Dependencies
-import numpy
-import random
+import numpy as np
 
 # Parameter
 k = [ 5, 10, 20 ] # Kontakte pro Person
@@ -17,7 +16,7 @@ d = 0.001 # An jedem weiteren Tag beträgt die Sterbewahrscheinlichkeit 0.1%
 
 # Adjazenzmatrix initialisieren
 def createAMatrix(k, n):
-  A = numpy.full((n, n), False)
+  A = np.full((n, n), False)
 
   # iterate through rows
   for i in range(n):
@@ -34,9 +33,9 @@ def createAMatrix(k, n):
       nNewContacts = min(nContactsPossible, nContactsRemaining)
 
       # hypergeometric distribution of contacts
-      newContacts = numpy.random.choice(numpy.concatenate([
-          numpy.full(nNewContacts, True),
-          numpy.full(nContactsPossible - nNewContacts, False)
+      newContacts = np.random.choice(np.concatenate([
+          np.full(nNewContacts, True),
+          np.full(nContactsPossible - nNewContacts, False)
       ]), nContactsPossible, replace=False)
 
       A[i, indexFilter] = newContacts
@@ -44,58 +43,58 @@ def createAMatrix(k, n):
 
   return A
 
-# Simulations-Funktion (Parameter k, m, p)
+# Simulations-Funktion (Parameter n, k, m, p)
 def simul_epidemic(n, k, m, p, isol=20):
 
   A = createAMatrix(k, n)
 
   # Krankheitsdauer (gleichverteilt zwischen 10 und 15 Tagen)
   # Tag 1 ist der Tag, an dem Person angesteckt wird - Genesung an Tag x+1 (Ansteckend bis Tag x)
-  duration = numpy.random.randint(10, 16, n)
+  duration = np.random.randint(10, 16, n)
 
   # Zeithorizont der Simulation (100 reicht anscheinend, mehr ist aber problemlos möglich)
   T = 100
   # Matrix mit den Simulationsergebnissen
-  results = numpy.full((T, n), "H")
+  results = np.full((T, n), "H")
   # Bisherige Dauer der Ansteckung
-  daysSick = numpy.full(n, 0, dtype = int)
+  daysSick = np.full(n, 0)
   # Neuinfektionen pro Tag
-  newInfections = numpy.full(T, 0, dtype = int)
+  newInfections = np.full(T, 0)
 
   # Initial infection at day 1
-  results[0] = numpy.concatenate([numpy.full(m, "D"), numpy.full(n - m, "H")])
+  results[0] = np.concatenate([
+      np.full(m, "D"),
+      np.full(n - m, "H")
+  ])
 
   for t in range(1, T):
-    sickPeople = numpy.where(results[t - 1] == "D")[0]
+    currentlySickPeople = np.where(results[t - 1] == "D")[0]
+    daysSick[currentlySickPeople] += 1
 
     # stop the simulation, if further infection is impossible
-    # if sickPeople.size <= 0:
+    # if currentlySickPeople.size <= 0:
     #   break
 
-    daysSick[sickPeople] += 1
-
-    # print(sickPeople)
-
-    for sickPerson in sickPeople:
-      if daysSick[sickPerson] > duration[sickPerson]:
+    for currentlySickPerson in currentlySickPeople:
+      if daysSick[currentlySickPerson] > duration[currentlySickPerson]:
         # simualtion of recovery
-        daysSick[sickPerson] = 0
-        results[t:T, sickPerson] = "R"
-      elif daysSick[sickPerson] >= fpd and random.random() < d:
+        daysSick[currentlySickPerson] = 0
+        results[t:T, currentlySickPerson] = "R"
+      elif daysSick[currentlySickPerson] >= fpd and np.random.random() < d:
         # simualtion of fatality
-        daysSick[sickPerson] = 0
-        results[t:T, sickPerson] = "T"
+        daysSick[currentlySickPerson] = 0
+        results[t:T, currentlySickPerson] = "T"
       else:
         # current person remains sick
-        results[t, sickPerson] = "D"
+        results[t, currentlySickPerson] = "D"
 
         # simulation of infection
-        contacts = numpy.where(A[sickPerson])[0]
+        contacts = np.where(A[currentlySickPerson])[0]
         for contact in contacts:
-          if results[t, contact] == "H" and results[t - 1, contact] == "H" and random.random() < p:
+          if results[t, contact] == "H" and results[t - 1, contact] == "H" and np.random.random() < p:
             results[t, contact] = "D"
             newInfections[t] += 1
 
   return results
 
-print(simul_epidemic(100, k[0], m[0], p[0], 20))
+print(simul_epidemic(100, k[0], m[0], p[0]))
