87e9c96e5372161eabb04ce75aeac23478cd74b2
peeterburger <peter2000.burger2000@gmail.com>
2020-08-17 17:31:44 +0200

    overall improvements, implemented matrix import/export

--- python/abschlussbeispiel.py
+++ python/abschlussbeispiel.py
@@ -1,28 +1,36 @@
-# Abschlussbeispiel Computational Statistics
-# Gruppe [Namen bitte einfügen]
-
-# Dependencies
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-# Parameter
-k = [ 5, 10, 20 ] # Kontakte pro Person
-m = [ 1, 5, 10 ] # Infizierte an Tag 1
-p = [ 0.1, 0.25, 0.5 ]
-n = 5000 # Anzahl der Personen im Modell
-fpd = 5 # Erster möglicher Tag, an dem man sterben kann
-isol = [ 7, 20 ] # Isolation ab Tag isol - 20 steht hierbei für keine Isolation (nachdem alle Kranken ab Tag 16 gesundet oder tot sind)
-d = 0.001 # An jedem weiteren Tag beträgt die Sterbewahrscheinlichkeit 0.1%
-# Das ergibt eine Sterbewahrscheinlichkeit von 1 - 0.999^5 bis 1 - 0.999^10 (im Durchschnitt ca. 0.75 %)
+# example parameters for a simulation
+n = 5000                # total number of people
+k = [ 5, 10, 20 ]       # number of contacts per person
+m = [ 1, 5, 10 ]        # number of people infected at day one
+p = [ 0.1, 0.25, 0.5 ]  # infection rate
+i = [ 7, 20 ]           # number of days that pass before an infected person isolates itself
+d = 0.001               # mortality rate per day
+fpd = 5                 # minimum number of days before the desease might become fatal
+minDaysInfected = 10    # minimum number of days a person might be infected
+maxDaysInfected = 15    # maximum number of days a person might be infected
 
-# Adjazenzmatrix initialisieren
-def createAMatrix(k, n):
-  A = np.full((n, n), False)
+# creates a contact matrix based on the total number of people and the number
+# of contacts per person
+def createContactMatrix(n, k, usingFiles=False):
+
+  filePath = "./data/%s_%s.npy" % (n, k)
+
+  if usingFiles:
+    try:
+      return np.load(filePath)
+    except IOError:
+      print("File %s not found. Generating new matrix..." % filePath)
+
+  contactMatrix = np.full((n, n), False)
 
   # iterate through rows
   for i in range(n):
 
-    nContactsRemaining = k - A[i].sum()
+    nContactsRemaining = k - contactMatrix[i].sum()
 
     # if no contacts have to be added, continue with the next person
     if nContactsRemaining <= 0:
@@ -30,7 +38,7 @@ def createAMatrix(k, n):
 
     # all j < i are automatically excluded, since their column sum is
     # always greater or equal k
-    indexFilter = A.sum(axis=0) < k
+    indexFilter = contactMatrix.sum(axis=0) < k
 
     # a person is not in contact with itself
     indexFilter[i] = False
@@ -48,37 +56,42 @@ def createAMatrix(k, n):
     chosenContacts = np.random.choice(possibleContacts, nNewContacts, replace=False)
 
     for contact in chosenContacts:
-      A[i, contact] = A[contact, i] = True
+      contactMatrix[i, contact] = contactMatrix[contact, i] = True
 
-  return A
+  if usingFiles:
+    np.save(filePath, contactMatrix)
 
-# Simulations-Funktion (Parameter n, m, p)
-def simul_epidemic(n, m, p, contactMatrix, isol=20):
+  return contactMatrix
 
-  # Krankheitsdauer (gleichverteilt zwischen 10 und 15 Tagen)
-  # Tag 1 ist der Tag, an dem Person angesteckt wird - Genesung an Tag x+1 (Ansteckend bis Tag x)
-  duration = np.random.randint(10, 16, n)
+# simulation of desease progression
+def simulateEpidemic(n, m, p, i, contactMatrix):
 
-  # Zeithorizont der Simulation (100 reicht anscheinend, mehr ist aber problemlos möglich)
+  # time horizon for the simulation
   T = 100
-  # Matrix mit den Simulationsergebnissen
+
+  # duration of the desease (per-person)
+  duration = np.random.randint(minDaysInfected, maxDaysInfected + 1, n)
+
+  # matrix that holds the status of each person for each unit of time
   results = np.full((T, n), "H")
-  # Bisherige Dauer der Ansteckung
+
+  # number of days each person is infected
   daysSick = np.full(n, 0)
 
-  # Initial infection at day 1
+  # initial 'm' infections at day 1
   results[0] = np.concatenate([
       np.full(m, "D"),
       np.full(n - m, "H")
   ])
 
+  # simulate all following days
   for t in range(1, T):
     currentlySickPeople = np.where(results[t - 1] == "D")[0]
     daysSick[currentlySickPeople] += 1
 
     # stop the simulation, if further infection is impossible
-    # if currentlySickPeople.size <= 0:
-    #   break
+    if currentlySickPeople.size <= 0:
+      break
 
     for currentlySickPerson in currentlySickPeople:
       if daysSick[currentlySickPerson] > duration[currentlySickPerson]:
@@ -89,8 +102,8 @@ def simul_epidemic(n, m, p, contactMatrix, isol=20):
         # simualtion of fatality
         daysSick[currentlySickPerson] = 0
         results[t:T, currentlySickPerson] = "T"
-      else:
-        # current person remains sick
+      elif daysSick[currentlySickPerson] <= i:
+        # person remains sick
         results[t, currentlySickPerson] = "D"
 
         # simulation of infection
@@ -101,50 +114,44 @@ def simul_epidemic(n, m, p, contactMatrix, isol=20):
 
   return results
 
+# returns a dataframe with usefull statistics extracted from the result of a simulation
 def evaluateResult(result):
   T = result.shape[0]
 
-  currentlyInfected = np.empty(T, dtype=int)
-  currentlyHealthy = np.empty(T, dtype=int)
-  currentlyResistant = np.empty(T, dtype=int)
-  currentlyDead = np.empty(T, dtype=int)
+  infected = np.empty(T, dtype=int)
+  healthy = np.empty(T, dtype=int)
+  resistant = np.empty(T, dtype=int)
+  deaths = np.empty(T, dtype=int)
   newInfections = np.empty(T, dtype=int)
 
   # basic statistics
-  for t, dailyStatistics in enumerate(result):
-    currentlyInfected[t] = (dailyStatistics == 'D').sum()
-    currentlyHealthy[t] = (dailyStatistics == 'H').sum()
-    currentlyResistant[t] = (dailyStatistics == 'R').sum()
-    currentlyDead[t] = (dailyStatistics == 'T').sum()
+  for t, dailyStats in enumerate(result):
+    infected[t] = (dailyStats == 'D').sum()
+    healthy[t] = (dailyStats == 'H').sum()
+    resistant[t] = (dailyStats == 'R').sum()
+    deaths[t] = (dailyStats == 'T').sum()
 
-  # new infections
-  for t, dailyStatistics in enumerate(result):
+  # new infections statistics
+  for t in range(T):
     if t == 0:
       newInfections[t] = (result[0] == 'D').sum()
     else:
       newInfections[t] = np.logical_and(
-          result[t - 1] == 'H',
-          result[t] == 'D'
+        result[t - 1] == 'H',
+        result[t] == 'D'
       ).sum()
 
-  return (currentlyInfected, currentlyHealthy,
-      currentlyResistant, currentlyDead, newInfections)
+  return pd.DataFrame.from_dict({
+    "infected": infected,
+    "healthy": healthy,
+    "resistant": resistant,
+    "number of deaths": deaths,
+    "daily new cases": newInfections
+  })
 
-def plotStatistics(statistics):
-  plt.plot(np.arange(100), statistics[0])
-  plt.plot(np.arange(100), statistics[1])
-  plt.plot(np.arange(100), statistics[2])
-  plt.plot(np.arange(100), statistics[3])
-  plt.plot(np.arange(100), statistics[4])
-  plt.xlabel("days")
-  plt.ylabel("people")
-  plt.savefig("plot3.pdf")
+contactMatrix = createContactMatrix(n, k[0], True)
 
-n = 2000
+result = simulateEpidemic(n, m[0], p[0], 10, contactMatrix)
 
-contactMatrix = createAMatrix(k[0], n)
-
-result = simul_epidemic(n, m[0], p[0], contactMatrix)
-statistics = evaluateResult(result)
-
-plotStatistics(statistics)
+evaluateResult(result).plot()
+plt.savefig('plot.pdf')
